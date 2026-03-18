564e5bf6f156d2a71e462b1be657eedcef0d0e90
aamoghS <aamoghsawantt@gmail.com>
2026-03-18 12:54:40 -0400

    tanstack is being used for ts shit

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -27,7 +27,7 @@ const itemVariants = {
     opacity: 1,
     scale: 1,
     y: 0,
-    transition: { duration: 0.4, ease: [0.4, 0, 0.2, 1] }
+    transition: { duration: 0.4, ease: [0.4, 0, 0.2, 1] as const }
   }
 };
 
